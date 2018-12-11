d98af6cd2ecb6775528a842e8c5e1ab444b3f772
Shawn Rupp <smrupp16@gmail.com>
2018-12-11 12:30:18 -0700

    Updated curatedTaxonomies to group entries with missing species names with corrected names

--- curatedTaxonomies.py
+++ curatedTaxonomies.py
@@ -33,13 +33,42 @@ class Queries():
 		self.header = None
 		self.delim = None
 		self.queries = {}
-		self.taxonomy = {}			
+		self.taxonomy = {}
+		self.total = 0
 
 	def __addTaxa__(self, row, name):
 		# Adds taxonomy to dict
 		end = min(len(row), self.header.corrected)
-		self.taxonomy[row[0]] = row[2:end]
+		self.taxonomy[row[0]] = row[1:end]
 		self.taxonomy[row[0]].append(name)
+
+	def __checkEntry__(self, s, name):
+		# Adds data from row to appropriate dict
+		if len(s) > self.header.corrected and len(s[self.header.corrected]) >= 2:
+			# Check corrected entry
+			if "?" not in s[self.header.corrected]:
+				# Skip unintelligable entries
+				spl = s[self.header.corrected].split()
+				if len(spl) != 2:
+					sp = " ".join(spl[:2])
+				else:
+					sp = s[self.header.corrected]
+				# Add corrected scientific name
+				self.queries[s[self.header.query]] = [sp, name]
+		else:
+			# Check existing scientific name
+			spl = s[self.header.species].split()
+			if len(spl) >= 2:
+				sp = spl[1].strip().lower()
+				if sp == "sp." or sp == "na":
+					# Add entries with missing species data to queries
+					self.queries[s[self.header.query]] = [s[self.header.species], name]
+				else:
+					# Keep well formatted names
+					self.__addTaxa__(s, name)
+			else:
+				# Add malformed entries to queries
+				self.queries[s[self.header.query]] = [s[self.header.species], name]
 
 	def readCSV(self, infile):
 		# Reads csv into dict
@@ -48,47 +77,41 @@ class Queries():
 		with open(infile, "r") as f:
 			for line in f:
 				if first == False:
-					s = line.split(self.delim)
-					if len(s) > self.header.corrected and len(s[self.header.corrected]) >= 2:
-						# Check corrected entry
-						if "?" not in s[self.header.corrected]:
-							# Skip unintelligable entries
-							spl = s[self.header.corrected].split()
-							if len(spl) != 2:
-								sp = " ".join(spl[:2])
-							else:
-								sp = s[self.header.corrected]
-							# Add corrected scientific name
-							self.queries[s[self.header.query]] = [sp, name]
-					else:
-						# Add existing scientific name
-						self.__addTaxa__(s, name)
+					self.total += 1
+					s = line.strip().split(self.delim)
+					self.__checkEntry__(s, name)
 				else:
 					if self.delim is None:
 						self.delim = getDelim(line)
 						row = line.split(self.delim)
 						self.header = Header(row)
 						row = row[:self.header.corrected]
-						row.append(name)
+						row.append("Name")
 						self.theader = ",".join(row) + "\n"
 					first = False
 
 	def writeQueries(self):
 		# Writes queries dict ot file
+		count = 0
 		print(("\tWriting queries to {}").format(self.queryout))
 		with open(self.queryout, "w") as out:
 			out.write(self.qheader)
 			for i in self.queries.keys():
 				row = ",".join([i, self.queries[i][0], "scientific", self.queries[i][1]])
 				out.write(row + "\n")
+				count += 1
+		print(("\tIdentified {:,} corrections from {:,} total records.").format(count, self.total))
 
 	def writeTaxa(self):
 		# Writes queries dict ot file
+		count = 0
 		print(("\tWriting taxonomies to {}").format(self.taxaout))
 		with open(self.taxaout, "w") as out:
 			out.write(self.theader)
 			for i in self.taxonomy.keys():
-				out.write(",".join(self.taxonomy[i]) + "\n")
+				out.write(("{},{}\n").format(i, ",".join(self.taxonomy[i])))
+				count += 1
+		print(("\tIdentified {:,} passing taxonomies from {:,} total records.").format(count, self.total))
 
 def mergeTaxa(indir, outdir):
 	# Reads each csv and adds to dict
