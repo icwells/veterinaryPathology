d6227f0b2a4a257ec51b27481403fe33f5b09698
Shawn Rupp <smrupp16@gmail.com>
2019-03-12 14:43:12 -0700

    Added updateRecords

--- updateRecords.sh
+++ updateRecords.sh
@@ -11,7 +11,7 @@ python mergeTaxonomy.py -t /home/shawn/Documents/Git/comparativeoncologydata/tax
 python mergeTaxonomy.py -t /home/shawn/Documents/Git/comparativeoncologydata/taxonomy.190312.csv -r /home/shawn/Documents/Git/comparativeoncologydata/NWZP/diagnoses/nwzpDiagnoses.csv -i /home/shawn/Documents/Git/comparativeoncologydata/NWZP/fulldata.UTF.csv -o /home/shawn/Documents/Git/comparativeoncologydata/NWZP/NWZP_fullRecords.csv
 
 	# Also update necropsies
-	python getEntries.py -c Necropsies -v Y -i /home/shawn/Documents/Git/comparativeoncologydata/NWZP/NWZP_fullRecords.csv -o /home/shawn/Documents/Git/comparativeoncologydata/NWZP/nwzpNecropsies.csv
+	python getEntries.py -c Necropsy -v Y -i /home/shawn/Documents/Git/comparativeoncologydata/NWZP/NWZP_fullRecords.csv -o /home/shawn/Documents/Git/comparativeoncologydata/NWZP/nwzpNecropsies.csv
 
 # ZEPS
 python mergeTaxonomy.py -t /home/shawn/Documents/Git/comparativeoncologydata/taxonomy.190312.csv -r /home/shawn/Documents/Git/comparativeoncologydata/ZEPS/ZEPS_diagnoses.csv -i /home/shawn/Documents/Git/comparativeoncologydata/ZEPS/ZEPS_data_clean.csv -o /home/shawn/Documents/Git/comparativeoncologydata/ZEPS/ZEPS_fullRecords.csv
